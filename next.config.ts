ea1257701d2493aa5c4c6c5bc226578a000883ce
gendonaja797-wq <gendonaja797@gmail.com>
2026-09-05 07:42:38 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
 import type { NextConfig } from "next";
 
-const repositoryName =
-  process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
-
-const isUserPagesRepository =
-  repositoryName.endsWith(".github.io");
-
-const basePath =
-  process.env.GITHUB_ACTIONS && !isUserPagesRepository
-    ? `/${repositoryName}`
-    : "";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath,
-  assetPrefix: basePath,
 };
 
 export default nextConfig;
